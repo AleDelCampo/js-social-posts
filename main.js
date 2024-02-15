faa84c83132250a7bafbbd78b9d851cfc003e908
AleHashirama <alehashirama@gmail.com>
2024-02-15 15:44:03 +0100

    manipolato il dom per la visualizzazione dei post tramite js. aggiunto anche fotina per avatar in uno dei membri che non ne aveva

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/*Descrizione
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+Milestone 2 - Prendendo come riferimento il layout di esempio presente nell"html, stampiamo i post del nostro feed.
+Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+BONUS
+Formattare le date in formato italiano (gg/mm/aaaa)
+Gestire l"assenza dell"immagine profilo con un elemento di fallback che contiene le iniziali dell"utente (es. Luca Formicola > LF).
+Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.*/
 const posts = [
     {
         "id": 1,
@@ -38,7 +55,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=46"
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -55,3 +72,51 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+function createPost(post) {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+
+    postElement.innerHTML = `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.author.image}">
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author.name}</div>
+                    <div class="post-meta__time">${post.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button js-like-button" href="#" data-postid="${post.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>
+    `;
+
+    return postElement;
+}
+
+function showPosts() {
+    const container = document.getElementById("container");
+    posts.forEach(function(post) {
+        const postElement = createPost(post);
+        container.appendChild(postElement);
+    });
+}
+
+showPosts();

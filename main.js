090bb1eba1fdc4ecea3ea0b488fdbdb3b87e3b88
AleHashirama <alehashirama@gmail.com>
2024-02-15 17:14:18 +0100

    creato il necessario per mettere o togliere il mi piace. aggiunto anche un cuoricino per i like

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 /*Descrizione
-Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
-Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
-id del post, numero progressivo da 1 a n
-nome autore,
-foto autore,
-data in formato americano (mm-gg-yyyy),
-testo del post,
-immagine (non tutti i post devono avere una immagine),
-numero di likes.
-Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
-Milestone 2 - Prendendo come riferimento il layout di esempio presente nell"html, stampiamo i post del nostro feed.
 Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 BONUS
 Formattare le date in formato italiano (gg/mm/aaaa)
@@ -103,6 +92,7 @@ function createPost(post) {
                 </div>
                 <div class="likes__counter">
                     Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                    <i id="hearth" class="like-icon fas fa-heart"></i>
                 </div>
             </div> 
         </div>
@@ -118,5 +108,48 @@ function showPosts() {
         container.appendChild(postElement);
     });
 }
-
 showPosts();
+
+
+
+const likedPost = [];
+
+function clickLike(like) {
+
+    like.preventDefault();
+
+    const likeButton = like.target.closest(".js-like-button");
+
+    const postId = Number(likeButton.dataset.postid);
+
+    const likeCounter = document.getElementById(`like-counter-${postId}`);
+
+    const actualLikes = Number(likeCounter.textContent);
+
+    const heartIcon = document.getElementById("hearth")
+
+    if (likedPost.includes(postId)) {
+
+        likeCounter.textContent = actualLikes - 1;
+
+        likeButton.classList.remove("like-button--liked");
+
+        heartIcon.classList.remove("liked");
+
+        const index = likedPost.indexOf(postId);
+
+        if (index > -1) {
+            likedPost.splice(index, 1);
+        }
+
+    } else {
+        likeCounter.textContent = actualLikes + 1;
+
+        likeButton.classList.add("like-button--liked");
+
+        heartIcon.classList.add("liked");
+        
+        likedPost.push(postId);
+    }
+}
+document.addEventListener("click", clickLike);
